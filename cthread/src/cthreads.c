ec8baec4fde2ee92efc86576b864beb74280d75e
Guilherme Tassinari <guitassinari@gmail.com>
2016-09-06 23:28:38 -0300

    Creates tid and ticket for threads

--- cthread/src/cthreads.c
+++ cthread/src/cthreads.c
@@ -1,10 +1,38 @@
 #include "../include/cthread.h"
 #include "../include/cdata.h"
 #include "../include/support.h"
+#include <stdlib.h>
 
-int ccreate (void* (*start)(void*), void *arg);
+#define MAX_TICKET 255
+
+int tid = 0;
+
+int ccreate (void* (*start)(void*), void *arg){
+    int threadTid = newTid();
+    TCB_t * newThread = malloc(sizeof(TCB_t));
+    newThread->ticket = newTicket();
+    newThread->state = PROCST_APTO;
+    newThread->tid = threadTid;
+
+    return threadTid
+}
+
 int cyield(void);
 int cjoin(int tid);
 int csem_init(csem_t *sem, int count);
 int cwait(csem_t *sem);
 int csignal(csem_t *sem);
+
+int newTid(){
+  int actualTid = tid;
+  tid++;
+  return actualTid;
+}
+
+int newTicket(){
+  return (rand() % 10) + (rand() % 1);
+}
+
+int teste(){
+  printf("uma função de teste");
+}
